f0206dc24d79daa9044a11c224584ecf9cf31a80
chfw <wangc_2011@hotmail.com>
2017-02-28 17:39:37 +0000

    support width

--- sphinxcontrib/excel.py
+++ sphinxcontrib/excel.py
@@ -15,14 +15,21 @@ import pyexcel
 
 class PyexcelTable(Directive):
     required_arguments = 1
-    optional_arguments = 0
+    optional_arguments = 1
     final_argument_whitespace = True
     has_content = True
 
     def run(self):
         excel_file = self.arguments[0]
+        if len(self.arguments) == 2:
+            width = self.arguments[1]
+            width = width.split(' ')[-1]
+            width = int(width)
+        else:
+            width = None
         sheet = pyexcel.get_book(file_name=excel_file)
-        handsontable = sheet.get_handsontable_html(embed=True)
+        handsontable = sheet.get_handsontable_html(embed=True,
+                                                   width=width)
         return [docutils.nodes.raw('', handsontable,
                                    format='html')]
 
