2d8184233e2e2d8e10938688557646bd170d5675
chfw <wangc_2011@hotmail.com>
2017-06-09 11:41:35 +0100

    support embeded csv

--- sphinxcontrib/excel.py
+++ sphinxcontrib/excel.py
@@ -15,25 +15,35 @@ import pyexcel
 
 
 class PyexcelTable(Directive):
-    required_arguments = 1
-    optional_arguments = 1
+    required_arguments = 0
+    optional_arguments = 2
     final_argument_whitespace = True
     has_content = True
 
     def run(self):
         env = self.state.document.settings.env
-        fn = search_image_for_language(self.arguments[0], env)
-        relfn, excel_file = env.relfn2path(fn)
-        env.note_dependency(relfn)
-        if len(self.arguments) == 2:
-            width = self.arguments[1]
-            width = width.split(' ')[-1]
-            width = int(width)
+        width = 600
+        if len(self.arguments) > 0:
+            fn = search_image_for_language(self.arguments[0], env)
+            relfn, excel_file = env.relfn2path(fn)
+            env.note_dependency(relfn)
+            if len(self.arguments) == 2:
+                width = self.arguments[1]
+                width = width.split(' ')[-1]
+                width = int(width)
+            book = pyexcel.get_book(file_name=excel_file)
         else:
-            width = None
-        sheet = pyexcel.get_book(file_name=excel_file)
-        handsontable = sheet.get_handsontable_html(embed=True,
-                                                   width=width)
+            content = '\n'.join(self.content)
+            if '---pyexcel' in content:
+                multiple_sheets = True
+            else:
+                multiple_sheets = False
+            book = pyexcel.get_book(file_content='\n'.join(self.content),
+                                    multiple_sheets=multiple_sheets,
+                                    lineterminator='\n',
+                                    file_type='csv')
+        handsontable = book.get_handsontable_html(embed=True,
+                                                  width=width)
         return [docutils.nodes.raw('', handsontable,
                                    format='html')]
 
