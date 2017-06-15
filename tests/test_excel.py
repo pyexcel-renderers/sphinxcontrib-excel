60a29bbbbf3fefe8117d0416bb7bd9b19cfb5b30
chfw <wangc_2011@hotmail.com>
2017-06-15 18:28:09 +0100

    fix broken tests

--- tests/test_excel.py
+++ tests/test_excel.py
@@ -1,13 +1,18 @@
 import os
-from mock import MagicMock
-from sphinxcontrib.excel import PyexcelTable, setup as s
+from mock import MagicMock, patch
+from sphinxcontrib.excel import setup as s
 from nose.tools import eq_
 
 
-def test_pyexcel_table_directive():
+@patch('sphinxcontrib.excel.search_image_for_language')
+def test_pyexcel_table_directive(fake_search):
+    from sphinxcontrib.excel import PyexcelTable
+    state = MagicMock()
+    fake_search.return_value = ''
     arguments = [os.path.join("tests", "fixtures", "test.csv")]
+    state.document.settings.env.relfn2path.return_value = (None, arguments[0])
     directive = PyexcelTable('test', arguments, None, None,
-                             None, None, None, None, None)
+                             None, None, None, state, None)
     x = directive.run()
     content = str(x[0])
     assert '<raw format="html" xml:space="preserve">' in content
